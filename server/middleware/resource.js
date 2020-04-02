c162b7a8416f1806d48987ddd767ae9ad993c5bc
Zhouqiaoqiao1026 <673077509@qq.com>
2020-04-02 17:16:44 +0800

    del

--- server/middleware/resource.js
+++
@@ -1,7 +0,0 @@
-module.exports=options=>{
-    return async(req,res,next)=>{
-        const modelName=require('inflection').classify(req.params.resource) //小写负数转化为大写单数
-        req.Model=require(`../models/${modelName}`)
-        next()
-    }
-}
--- server/models/Ad.js
+++
@@ -1,5 +0,0 @@
-const mongoose=require('mongoose')
-const schema=new mongoose.Schema({
-    image:{type:String}
-})
-module.exports=mongoose.model('Ad',schema)  //导出模型
