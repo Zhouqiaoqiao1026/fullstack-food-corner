d2fc26f082e6a4a7f3e866971310dc8b13636de8
Zhouqiaoqiao1026 <673077509@qq.com>
2020-04-02 21:39:46 +0800

    fix upload url

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -47,7 +47,7 @@ module.exports=app=>{
     const upload =multer({dest:__dirname+'/../../uploads'})
 app.post('/admin/api/upload',authMiddleware(),upload.single('file'),async(req,res)=>{
     const file=req.file;
-    file.url=`http://localhost:3001/uploads/${file.filename}`
+    file.url=`http://test.bol4.top/uploads/${file.filename}`
     res.send(file);
 })
 
